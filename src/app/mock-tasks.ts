e352634ce00390ecf8fd992894990afe16d48c9e
Richard Ferreira <56368223+JLArichardferreira@users.noreply.github.com>
2021-12-01 17:26:35 -0600

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Richard Grooming',
+    day: 'December 6th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Thanksgiving',
+    day: 'June 1st at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Gaming',
+    day: 'May 8th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Sleeping',
+    day: 'May 10th at 2:30pm',
+    reminder: true,
+  },
+];
